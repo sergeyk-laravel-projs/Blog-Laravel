87aef59fc33a73548b9e3c18a33999cd1a29924f
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-03-24 21:44:13 +0000

    33_Сохранение нового материала в базу данных. ч1

--- blog.laravel.loc/app/Http/Controllers/Admin/ArticlesController.php
+++ blog.laravel.loc/app/Http/Controllers/Admin/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace Corp\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Corp\Http\Requests\ArticleRequest;
 
 use Corp\Http\Requests;
 use Corp\Http\Controllers\Controller;
@@ -99,9 +100,16 @@ class ArticlesController extends AdminController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
         //
+		$result = $this->a_rep->addArticle($request);
+		
+		if(is_array($result) && !empty($result['error'])) {
+			return back()->with($result);
+		}
+		
+		return redirect('/admin')->with($result);
     }
 
     /**
